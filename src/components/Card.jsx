22d790c004df89dd790c0f943dc5a4f47222f618
giovannimazzi <gioelor@libero.it>
2026-02-23 13:06:28 +0100

    add type and image in Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,15 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Card({ name, url }) {
-  const [pokemonDetails, setPokemonDetails] = useState();
+  const [pokemonDetails, setPokemonDetails] = useState(undefined);
 
   useEffect(() => {
     axios
       .get(url)
       .then((res) => {
-        setPokemonDetails(res.data);
-        console.log(res.data);
+        const imageUrl = res.data?.sprites.front_default;
+        const principalType = res.data?.types[0].type.name;
+        setPokemonDetails({ imageUrl, principalType });
       })
       .catch((err) => console.log(`ERRORE: ${err.message}`));
   }, []);
@@ -19,6 +20,14 @@ export default function Card({ name, url }) {
       <div className="col">
         <div className="card p-2">
           <h1>{name}</h1>
+          {pokemonDetails && (
+            <>
+              <img src={pokemonDetails.imageUrl} alt={name} />
+              <h2 className="text-muted text-center">
+                {pokemonDetails.principalType}
+              </h2>
+            </>
+          )}
         </div>
       </div>
     </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,35 +3,34 @@ import axios from "axios";
 import Card from "../components/Card";
 
 export default function HomePage() {
-
   const [pokemonList, setPokemonList] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    axios.get('https://pokeapi.co/api/v2/pokemon')
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
       .then((res) => {
         setPokemonList(res.data.results);
         console.log(res.data.results);
       })
-      .catch((err) => {console.error(err.message);})
-      .finally(()=>{
+      .catch((err) => {
+        console.error(err.message);
+      })
+      .finally(() => {
         setLoading(false);
       });
-
-
-  }, [])
+  }, []);
 
   return (
     <>
-      {loading && <div>Loading...</div>
-      }
+      {loading && <div>Loading...</div>}
       <h1 className="my-5">Home page</h1>
-      <div className="row g-3">
+      <div className="row row-cols-3 g-3">
         {pokemonList.map((pokemon) => {
           return (
-            <Card key={pokemon.name} name={pokemon.name} url={pokemon.url}/>
-          )
+            <Card key={pokemon.name} name={pokemon.name} url={pokemon.url} />
+          );
         })}
       </div>
     </>
