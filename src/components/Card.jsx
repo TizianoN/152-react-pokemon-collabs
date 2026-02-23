b3d60395be246fe6697eaabcd0d95199cd0d97cf
giovannimazzi <gioelor@libero.it>
2026-02-23 12:40:55 +0100

    add details fetching

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Card({ name, url }) {
-    return <>
-        <div className="col">
-            <div className="card p-2">
-                <h1>{name}</h1>
-            </div>
+  const [pokemonDetails, setPokemonDetails] = useState();
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setPokemonDetails(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(`ERRORE: ${err.message}`));
+  }, []);
+
+  return (
+    <>
+      <div className="col">
+        <div className="card p-2">
+          <h1>{name}</h1>
         </div>
+      </div>
     </>
+  );
 }
