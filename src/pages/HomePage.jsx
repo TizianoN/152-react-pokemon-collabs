14834f6e3f2bb9820d8e97a63910edd39a836374
micheleferri01 <micheleferri095@gmail.com>
2026-02-23 11:10:35 +0100

    create: stato di loading

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 
 export default function HomePage() {
+
+ const [pokemonList, setPokemonList] = useState();
+ const [loading , setLoading] = useState(false);
+
+ useEffect(() => {
+  
+ },[])
+
   return (
     <>
+    {loading && <div>Loading...</div>
+      }
       <h1 className="my-5">Home page</h1>
     </>
   );
