a7a5857ffa8e71894b89e91724f49d41a2a0442c
TizianoN <hello@tizianonicolai.com>
2026-02-23 10:31:42 +0100

    fix: commented bs icons import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import "bootstrap-icons/font/bootstrap-icons.css";
+// import "bootstrap-icons/font/bootstrap-icons.css";
 
 import "./assets/css/index.css";
 import App from "./App.jsx";
