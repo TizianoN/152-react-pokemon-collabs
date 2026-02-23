f3b7bab2fef369655187f2dda3425e14f78b2412
TizianoN <hello@tizianonicolai.com>
2026-02-23 10:23:48 +0100

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
     <>
+      <Navbar />
       <div className="container">
         <h1 className="my-5">PokeAPI project</h1>
       </div>
