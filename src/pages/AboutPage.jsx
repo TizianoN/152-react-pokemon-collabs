6747a820638391dafe60897befab850adfa20dd6
TizianoN <hello@tizianonicolai.com>
2026-02-23 10:42:51 +0100

    create pages

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div className="container">
+      <h1 className="my-5">About page</h1>
+    </div>
+  );
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="container">
+      <h1 className="my-5">Home page</h1>
+    </div>
+  );
+}
