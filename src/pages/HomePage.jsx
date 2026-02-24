a2d3bc2bd03fa20cc13d045a06051be668063541
TizianoN <hello@tizianonicolai.com>
2026-02-24 09:50:21 +0100

    add env

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "../components/Card";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export default function HomePage() {
   const [pokemonList, setPokemonList] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -9,7 +11,7 @@ export default function HomePage() {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("https://pokeapi.co/api/v2/pokemon")
+      .get(apiUrl + "/pokemon")
       .then((res) => {
         setPokemonList(res.data.results);
         console.log(res.data.results);
@@ -28,9 +30,7 @@ export default function HomePage() {
       <h1 className="my-5">Home page</h1>
       <div className="row row-cols-3 g-3">
         {pokemonList.map((pokemon) => {
-          return (
-            <Card key={pokemon.name} name={pokemon.name} url={pokemon.url} />
-          );
+          return <Card key={pokemon.name} name={pokemon.name} url={pokemon.url} />;
         })}
       </div>
     </>
